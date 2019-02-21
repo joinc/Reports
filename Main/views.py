e223df25ed2418f8dc962c67dd9e8f62aa12bc01
Yuriy Belyakov <yura.belyakov@gmail.com>
2019-02-22 00:29:16 +0600

    Форма входа и выхода

--- Main/views.py
+++ Main/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.views.generic.edit import FormView
+from django.conf import settings
+
+
+######################################################################################################################
+
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+
+    return render(request, 'index.html', {})
+
+
+######################################################################################################################
+
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+
+    # Аналогично регистрации, только используем шаблон аутентификации.
+    template_name = "login.html"
+    # В случае успеха перенаправим на главную.
+    success_url = settings.SUCCESS_URL
+
+    def form_valid(self, form):
+        # Получаем объект пользователя на основе введённых в форму данных.
+        self.user = form.get_user()
+        # Выполняем аутентификацию пользователя.
+        login(self.request, self.user)
+
+        return super(LoginFormView, self).form_valid(form)
+
+
+######################################################################################################################
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
--- Reports/urls.py
+++ Reports/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Main import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index', ),
+    path('login/', views.LoginFormView.as_view(), name='login', ),
+    path('logout/', views.logout_view, name='logout', ),
 ]
